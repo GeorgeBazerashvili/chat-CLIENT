c9058a21ea09b688f0321a59314e292d2438dd05
George Bazerashvili <georgebazerashvili200@gmail.com>
2024-01-04 14:36:40 +0400

    changed back to localhsot

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 export const AppSource = createContext(null);
 
 function App() {
-  axios.defaults.baseURL = "https://chat-rest-production.up.railway.app";
+  axios.defaults.baseURL = "http://localhost:3000";
   const [loggedIn, setLoggedIn] = useState(
     !!localStorage.getItem("accessToken")
   );
